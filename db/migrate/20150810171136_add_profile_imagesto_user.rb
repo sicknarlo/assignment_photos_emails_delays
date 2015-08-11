63d390e39a832fd7cbaffabb3bf2cccb700fdffd
facingsouth <xin.d.liu@gmail.com>
2015-08-11 09:53:55 -0700

    merge

--- db/migrate/20150810171136_add_profile_imagesto_user.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileImagestoUser < ActiveRecord::Migration
-  def change
-    add_column :users, :profile_photo_path, :string
-  end
-end
--- db/migrate/20150810205356_add_profile_pic_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProfilePicColumnToUsers < ActiveRecord::Migration
-  def change
-    add_attachment :users, :profile_pic
-  end
-end
